d09ed16ef1ef75fa9394c19d804ee2b7a30c247c
DevonThomassen <devon.thomassen@hotmail.com>
2019-09-27 21:45:57 +0200

    Forgot BindableBase implement

--- MVVMHierarchiesDemo/ViewModel/CustomerListViewModel.cs
+++ MVVMHierarchiesDemo/ViewModel/CustomerListViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MVVMHierarchiesDemo.ViewModel
 {
-    class CustomerListViewModel
+    class CustomerListViewModel : BindableBase
     {
     }
 }
--- MVVMHierarchiesDemo/ViewModel/OrderViewModel.cs
+++ MVVMHierarchiesDemo/ViewModel/OrderViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MVVMHierarchiesDemo.ViewModel
 {
-    class OrderViewModel
+    class OrderViewModel : BindableBase
     {
     }
 }
